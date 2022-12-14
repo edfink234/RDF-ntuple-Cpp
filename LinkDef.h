e39d9e477a8c5d39ee459489e0770f8a28b57c53
System Administrator <root@eduroam-117-61.zdv.Uni-Mainz.DE>
2022-12-14 15:25:29 +0100

    Adding missing Linkdef file

---
+++ LinkDef.h
@@ -0,0 +1,6 @@
+#include <vector>
+#ifdef __ROOTCLING__
+#pragma link C++ class vector<vector <float> >+;
+#pragma link C++ class vector<vector <int> >+;
+#pragma link C++ class vector<vector <string> >+;
+#endif
