fc5c01fa558f5ce83a30cccfc6c734c916ab68dd
edfink234 <edward.finkelstein@cern.ch>
2023-06-22 09:07:33 +0200

    commit

--- Section7_TablesPlots.cxx
+++ Section7_TablesPlots.cxx
@@ -4663,7 +4663,7 @@ void Resolved_Up_Higgs_Mass()
                 errorHist->GetYaxis()->SetTitle("#frac{syst-nominal}{nominal}");
                 errorHist->GetYaxis()->SetTitleOffset(0.4);
                 errorHist->GetYaxis()->CenterTitle(true);
-                errorHist->SetMaximum(10000);
+                errorHist->SetMaximum(1e7);
                 errorHist->SetMinimum(-2.2);
                 errorHist->Draw("histsame");
 
@@ -5472,7 +5472,7 @@ void Resolved_Down_Higgs_Mass()
                 errorHist->GetYaxis()->SetTitle("#frac{syst-nominal}{nominal}");
                 errorHist->GetYaxis()->SetTitleOffset(0.4);
                 errorHist->GetYaxis()->CenterTitle(true);
-                errorHist->SetMaximum(46);
+                errorHist->SetMaximum(1e7);
                 errorHist->SetMinimum(-2.2);
                 errorHist->Draw("histsame");
 
@@ -5589,7 +5589,7 @@ void Resolved_Down_Higgs_Mass()
                 errorHist->GetYaxis()->SetTitle("#frac{syst-nominal}{nominal}");
                 errorHist->GetYaxis()->SetTitleOffset(0.4);
                 errorHist->GetYaxis()->CenterTitle(true);
-                errorHist->SetMaximum(3.8);
+                errorHist->SetMaximum(3.6);
                 errorHist->SetMinimum(-1.2);
                 errorHist->Draw("histsame");
 
